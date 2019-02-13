12cfb115a712395f4dfb90286eadbbf856d17f48
Eyal Toledano <eyaltoledano@me.com>
2019-02-13 23:19:25 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -44,7 +44,6 @@ def reformat_languages(languages)
 
       attributes.each do |type, type_value|
         new_hash.each do |language, data|
-          binding.pry
           data[:style] = [master_type]
         end
       end
